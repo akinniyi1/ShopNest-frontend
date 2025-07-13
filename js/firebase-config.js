1659da985cdd145fbb790b31392be64cefb44623
akinniyi1 <64546693+akinniyi1@users.noreply.github.com>
2025-07-13 23:55:24 +0100

    Delete js/firebase-config.js

--- js/firebase-config.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyBx_2_7aqxU5z6sLbbBp0fpdJvOzjarmGE",
-  authDomain: "shopnest-4cbdf.firebaseapp.com",
-  projectId: "shopnest-4cbdf",
-  storageBucket: "shopnest-4cbdf.firebasestorage.app",
-  messagingSenderId: "106375353281",
-  appId: "1:106375353281:web:cc6d19ddbef840e5715540",
-  measurementId: "G-FJ5TFZD45F"
-};
